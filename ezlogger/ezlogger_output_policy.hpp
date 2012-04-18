629686db6754b719a86363515e73cdb46cd5643a
winnux <lihaibo@126.com>
2012-04-18 13:51:17 +0800

    use absolute directory for log file

--- ezlogger/ezlogger_output_policy.hpp
+++ ezlogger/ezlogger_output_policy.hpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <string.h>
 #include <stdio.h>
 #include <time.h>
 
@@ -30,16 +31,38 @@ namespace axter
             static std::string ModuleFileName = GetRunningProgramName() + "_log.txt";
 			return ModuleFileName;
 		}
+#else
+
+                inline static std::string GetRunningProgramDir()
+                {
+                        char ModuleFileName[256] = "";
+                        readlink("/proc/self/exe",ModuleFileName,256);
+                        return ModuleFileName;
+                }
+                inline static std::string GetFileName()
+                {
+                    static char dir[256] ;
+                    strcpy(dir,GetRunningProgramDir().c_str());
+                    char* p = strrchr(dir,'/');
+                    if(p)
+                    {
+                        int k = strlen(dir);
+                        int n = strlen(p);
+                        dir[k-n]='\0';
+                        strcat(dir,"/log.txt");
+                    }
+            //static std::string ModuleFileName = GetRunningProgramName() + "_log.txt";
+                        return dir;
+                }
+
+//#ifndef EZLOGGER_OUTPUT_FILENAME
+//#define EZLOGGER_OUTPUT_FILENAME "log_output.txt"
+//#endif
+#endif
 #ifndef EZLOGGER_OUTPUT_FILENAME
 #define EZLOGGER_OUTPUT_FILENAME GetFileName()
 #endif //EZLOGGER_OUTPUT_FILENAME
-#endif //_WIN32
 
-
-#ifndef EZLOGGER_OUTPUT_FILENAME
-#define EZLOGGER_OUTPUT_FILENAME "log_output.txt"
-#endif
-	
 	public:
 		inline static std::ostream& get_log_stream()
 		{
